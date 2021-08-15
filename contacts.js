f06aa3a79cdc7aff8023f80a7034f9b8a74a2941
Aleksey Bogush <71962603+Bogushukr@users.noreply.github.com>
2021-08-15 14:47:52 +0300

    'fix'

--- contacts.js
+++ contacts.js
@@ -1,64 +1,52 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const contactsPath = path.join(__dirname, 'db/contacts.json');
 
-function listContacts() {
-  fs.readFile(contactsPath, 'utf8', function (error, data) {
-    if (error) {
-      throw error;
-    }
-    const contacts = JSON.parse(data);
-    console.table(contacts);
-  });
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function getContactById(contactId) {
-  fs.readFile(contactsPath, 'utf8', function (error, data) {
-    if (error) {
-      throw error;
-    }
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
     const contacts = JSON.parse(data);
-    const contact = contacts.find(({ id }) => id === Number(contactId));
-    console.table(contact);
-  });
+    const contact = contacts.find(({ id }) => `${id}` === `${contactId}`);
+    return contact;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function removeContact(contactId) {
-  fs.readFile(contactsPath, 'utf8', function (error, data) {
-    if (error) {
-      throw error;
-    }
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
     const contacts = JSON.parse(data);
-    const filteredContacts = contacts.filter(({ id }) => id !== Number(contactId));
+    const filteredContacts = contacts.filter(({ id }) => `${id}` !== `${contactId}`);
     const beautifyContacts = JSON.stringify(filteredContacts, null, 4);
-
-    fs.writeFile(contactsPath, beautifyContacts, function (error) {
-      if (error) {
-        throw error;
-      }
-      console.table(filteredContacts);
-    });
-  });
+    await fs.writeFile(contactsPath, beautifyContacts);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  fs.readFile(contactsPath, 'utf8', function (error, data) {
-    if (error) {
-      throw error;
-    }
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
     const contacts = JSON.parse(data);
     const id = Math.max(...contacts.map(({ id }) => id)) + 1;
     const contact = { id, name, email, phone };
     const updatedContacts = [...contacts, contact];
     const newContacts = JSON.stringify(updatedContacts, null, 4);
-    fs.writeFile(contactsPath, newContacts, function (error) {
-      if (error) {
-        throw error;
-      }
-      console.table(updatedContacts);
-    });
-  });
+    await fs.writeFile(contactsPath, newContacts);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -9,32 +9,43 @@ program
   .option('-e, --email <type>', 'user email')
   .option('-p, --phone <type>', 'user phone');
 
-
 program.parse(process.argv);
 
-const argv = program.opts();
+(async () => {
+  const argv = program.opts();
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      listContacts();
-      break;
+  async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case 'list': {
+        const data = await listContacts();
+        console.table(data);
+        break;
+      }
 
-    case 'get':
-      getContactById(id);
-      break;
+      case 'get': {
+        const data = await getContactById(id);
+        console.table(data);
+        break;
+      }
 
-    case 'add':
-      addContact(name, email, phone);
-      break;
+      case 'add': {
+        await addContact(name, email, phone);
+        const data = await listContacts();
+        console.table(data);
+        break;
+      }
 
-    case 'remove':
-      removeContact(id);
-      break;
+      case 'remove': {
+        await removeContact(id);
+        const data = await listContacts();
+        console.table(data);
+        break;
+      }
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
   }
-}
 
-invokeAction(argv);
+  await invokeAction(argv);
+})();
